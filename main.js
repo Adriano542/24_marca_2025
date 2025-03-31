4bd1fc22fef4a7da0468f9ac77173793a2aed226
Adriano <122603292+Adriano542@users.noreply.github.com>
2025-03-31 12:13:53 +0200

    v

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 // const mainHeader= document.getElementById('mainHeader')
 const mainHeader= document.querySelector('#mainHeader')
-
+console.log('Nagłówek główny: ', mainHeader);
 // const articleTitles= document.getElementsByTagName('h2')
 
-const articleTitles=document.querySelectorAll('article h2')
+mainHeader.innerHTML = '<h1>Mój artykuł</h1>'
+mainHeader.style.color = '#f00'
+mainHeader.style.backgroundColor='#ff0'
 
+const articleTitles=document.querySelectorAll('article h2')
 for(let i=0; i<articleTitles.length; i++){
     console.log(articleTitles[i])
+    articleTitles[i].textContent = 'Art. 0${i+1}'
 }
+
+
+
+const firstArticleHeader = document.querySelector('.article_header');
+console.log("Nagłówek pierwszego artykułu", firstArticleHeader);
+
+
+
